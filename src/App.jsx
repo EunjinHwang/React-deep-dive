75c63a986e747bf6f257101f1d04955876b4643d
Eunjin <melong0523486@gmail.com>
2025-01-14 23:13:28 -1200

    remove unnecessary states and create a 'gameTurns' state that can also be used in the Log component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import Players from "./components/Players";
+import { useState } from "react";
+import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from './components/Log'
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  function handleSelectSqure(rowIndex, colIndex){
+    setActivePlayer((currentActivePlayer)=> currentActivePlayer === "X" ? "O" : "X");
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      if(prevTurns.length > 0 && prevTurns[0].player === "X"){
+        currentPlayer = "O";
+      }
+      const updatedTurns = [{square: { row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns]
+      return updatedTurns;
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
-        <Players />
-
-        <GameBoard />
+      <ol id="players" className="highlight-player">
+          <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSqure} activePlayerSymbol = {activePlayer}/>
       </div>
       <div id="log">
         log
       </div>
+      <Log />
     </main>
   );
 }
