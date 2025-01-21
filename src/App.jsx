b14459bdaa37f9c9cc10db82d2551803c463483d
Eunjin <melong0523486@gmail.com>
2025-01-20 19:54:12 -0500

    Removed unnecessary activePlayer state and derived it from the gameTurns state

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from './components/Log'
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+  if(gameTurns.length > 0 && gameTurns[0].player === "X"){
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
+  let activePlayer = deriveActivePlayer(gameTurns);
+  
   function handleSelectSqure(rowIndex, colIndex){
-    setActivePlayer((currentActivePlayer)=> currentActivePlayer === "X" ? "O" : "X");
 
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if(prevTurns.length > 0 && prevTurns[0].player === "X"){
-        currentPlayer = "O";
-      }
+      let currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [{square: { row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns]
       return updatedTurns;
     });
